d603378e272240b73ec978b51e51f314168f9eac
averyawesome555 <32853959+averyawesome555@users.noreply.github.com>
2019-03-02 22:29:26 -0500

    Update Pawn.java

--- Pawn.java
+++ Pawn.java
@@ -1,78 +1,48 @@
 //Class: Chess
-//Written By: Carlo Favaro
+//Written By: Jack Shapiro
 //Date: 2/26/19
 //description: This class extends piece and contains the restrictions of the pawn's moves.
 
-import java.net.URL;
-import javax.swing.ImageIcon;
-
 public class Pawn extends Piece{
-
-	private ImageIcon image;
-	private int player;
-
 
 	public Pawn(int player) {
 		super(player, "images2/Pawn" + player+ ".png");
 	}
 
 
-
 	public boolean isValidMove(Location from, Location to, Piece[][]b){
 
 		//white team
 
-		
-
-		//if pawn is not in its first spot and move one spot up
-
-		if(getPlayer()==1 && from.getRow()!=7 && (from.getRow()==to.getRow()+1 || 
-
-		//pawn moving diagonal top left
-
-		(from.getRow()==to.getRow()+1)&&(from.getColumn()==to.getColumn()+1))||
-
-		//pawn moving diagonal top right
-
-		(from.getRow()==to.getRow()-1)&&(from.getColumn()==to.getColumn()-1) ||
-
-		//pawn moves twice when in first spot
-
-		from.getRow()==7 && from.getRow()==to.getRow()+2) {
-
+		if(getPlayer() == 1 && 
+		// moving forward general		
+		(to.getRow() == from.getRow() && to.getColumn() == from.getColumn() + 1 && b[to.getRow()][to.getColumn()] == null) ||
+		// moving forward 2 on first move
+		(from.getColumn() == 1 && to.getRow() == from.getRow() && to.getColumn() == from.getColumn() + 2 && b[to.getRow()][to.getColumn()] == null) ||
+		// kill to the right								// makes sure you actually want to kill that piece		
+		(b[from.getRow() + 1][from.getColumn() + 1] != null && b[from.getRow() + 1][from.getColumn() + 1].getPlayer() == 2 && to.getRow() == from.getRow() + 1 && to.getColumn() == from.getColumn() + 1 ) || 
+		// "catch and kill" to the left								// makes sure you actually want to kill that piece
+		(b[from.getRow() - 1][from.getColumn() + 1] != null && b[from.getRow() - 1][from.getColumn() + 1].getPlayer() == 2 && (to.getRow() == from.getRow() - 1 && to.getColumn() == from.getColumn() + 1) )		
+				) {
 				return true;
-
 			}
 
 
 
-		//Black Team
+		//black Team
 
-		
-
-			//if pawn is not in its first spot and move one spot up
-
-			if((getPlayer()==2 && from.getRow()!=1 && (from.getRow()==to.getRow()-1) || 
-
-			//pawn moving diagonal bottom left
-
-			((from.getRow()==to.getRow()-1)&&(from.getColumn()==to.getColumn()+1)))||
-
-			//pawn moving diagonal bottom right
-
-			((from.getRow()==to.getRow()-1)&&(from.getColumn()==to.getColumn()-1)) ||
-
-			//pawn moving twice in first move
-
-			from.getRow()==1 && from.getRow()==to.getRow()-2) {
-
+		else if(getPlayer() == 2 && 
+		// moving forward general		
+		(to.getRow() == from.getRow() && to.getColumn() == from.getColumn() - 1 && b[to.getRow()][to.getColumn()] == null) ||
+		// moving forward 2 on first move
+		(from.getColumn() == 6 && to.getRow() == from.getRow() && to.getColumn() == from.getColumn() - 2 && b[to.getRow()][to.getColumn()] == null) ||
+		// "catch and kill" to the right								// makes sure you actually want to kill that piece		
+		(b[from.getRow() + 1][from.getColumn() - 1] != null && b[from.getRow() + 1][from.getColumn() - 1].getPlayer() == 1 && to.getRow() == from.getRow() + 1 && to.getColumn() == from.getColumn() - 1 ) || 
+		// "catch and kill" to the left								// makes sure you actually want to kill that piece
+		(b[from.getRow() - 1][from.getColumn() - 1] != null && b[from.getRow() - 1][from.getColumn() - 1].getPlayer() == 1 && (to.getRow() == from.getRow() - 1 && to.getColumn() == from.getColumn() - 1) )		
+				) {
 				return true;
-
 			}
-
-		
-
-
 
 		return false;
 
