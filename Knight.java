6fe4034a6262690aee9578cb77bdb9ff33680182
Acemcshlaghg <Acemcshlaghg@users.noreply.github.com>
2019-03-03 17:19:02 -0500

    Update Knight.java

--- Knight.java
+++ Knight.java
@@ -1,10 +1,13 @@
 
 public class Knight extends Piece{
 	public Knight(int player) {
-		super(player, "images2/Knight" + player+ ".png");
+		super(player, "images2/Knight" + player + ".png");
 	}
 	public boolean isValidMove(Location from, Location to, Piece[][] b) {
-		if(b[to.getRow()][to.getColumn()]== null || b[to.getRow()][to.getColumn()].getPlayer() != getPlayer()) {
+		if(b[to.getColumn()][to.getRow()] != null && b[to.getColumn()][to.getRow()].getPlayer() != getPlayer()) {
+			return true;
+		}
+		else if(b[to.getColumn()][to.getRow()] == null) {
 			return true;
 		}
 		else {
