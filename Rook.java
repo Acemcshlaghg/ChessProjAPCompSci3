1f1ee4c398c018027d92269493e97e6899954e11
Acemcshlaghg <Acemcshlaghg@users.noreply.github.com>
2019-03-03 20:02:20 -0500

    Update Rook.java

--- Rook.java
+++ Rook.java
@@ -1,41 +1,50 @@
 
-public class Rook extends Piece{
+public class Rook extends Piece {
 
-	public Rook(int player){
+	public Rook(int player) {
 		super(player, "images2/Rook" + player + ".png");
 	}
-	
-	public boolean isValidMove(Location from, Location to, Piece[][]b){
-		if (b[to.getRow()][to.getColumn()] != null && b[to.getRow()][to.getColumn()].getPlayer() == getPlayer())
+
+	public boolean isValidMove(Location from, Location to, Piece[][] b) {
+		if (b[to.getColumn()][to.getRow()] != null && b[to.getColumn()][to.getRow()].getPlayer() == getPlayer())
 			return false;
-		
-		if(to.getRow()==from.getRow() && to.getColumn()>from.getColumn()) {
-			for(int i=from.getColumn()+1; i<=to.getColumn(); i++)
-				if(b[to.getRow()][i] != null)
+		// moving vertically-down
+		if (to.getRow() == from.getRow() && to.getColumn() > from.getColumn()) {
+			for (int i = from.getColumn() + 1; i < to.getColumn(); i++)
+				if (b[i][to.getRow()] != null)
 					return false;
 		}
-		else if(to.getRow()==from.getRow() && to.getColumn()<from.getColumn()){
-			for(int i=from.getColumn()-1; i>=to.getColumn(); i--)
-				if(b[to.getRow()][i] != null)
+		// moving vertically-up
+		else if (to.getRow() == from.getRow() && to.getColumn() < from.getColumn()) {
+			for (int i = from.getColumn()-1; i > to.getColumn(); i--)
+				if (b[i][to.getRow()] != null)
 					return false;
 		}
-		else if(to.getColumn()==from.getColumn() && to.getRow()<from.getRow()){
-			for(int i=from.getRow()-1; i>=to.getRow(); i--)
-				if(b[i][to.getColumn()] != null)
+		// moving horizontally-left
+		else if (to.getColumn() == from.getColumn() && to.getRow() < from.getRow()) {
+			for (int i = from.getRow() - 1; i > to.getRow(); i--)
+				if (b[to.getColumn()][i] != null)
 					return false;
 		}
-		else if(to.getColumn()==from.getColumn() && to.getRow()>from.getRow()){
-			for(int i=from.getRow()+1; i<=to.getRow(); i--)
-				if(b[i][to.getColumn()] != null)
+		// moving horizontally-Right
+
+		else if (to.getColumn() == from.getColumn() && to.getRow() > from.getRow()) {
+			for (int i = from.getRow() + 1; i < to.getRow(); i++)
+				if (b[to.getColumn()][i] != null)
 					return false;
 		}
-		else if(to.getColumn()==from.getColumn() && to.getRow()==from.getRow()){
+
+		else if (b[to.getColumn()][to.getRow()] != null
+				&& b[to.getColumn()][to.getRow()].getPlayer() == getPlayer()) {
 			return false;
+		} else if (b[to.getColumn()][to.getRow()] != null
+				&& b[to.getColumn()][to.getRow()].getPlayer() != getPlayer()) {
+			return false;
+		} else if (b[to.getColumn()][to.getRow()] == null) {
+			return true;
 		}
-		
-		return true;
-		
 	
+			return true;
 	}
-	
+
 }
