3e5c33a120dea94f6d97967dcf31ecc82d0ababb
Acemcshlaghg <Acemcshlaghg@users.noreply.github.com>
2019-03-03 21:56:32 -0500

    Update Pawn.java

--- Pawn.java
+++ Pawn.java
@@ -11,55 +11,59 @@ public class Pawn extends Piece {
 
 	public boolean isValidMove(Location from, Location to, Piece[][] b) {
 
+		if ((getPlayer() == 1 &&
 
-		if ((getPlayer() == 1 && 
-
-				// white team
-				// moving forward 1 on first move
+		// white team
+		// moving forward 1 on first move
 				((from.getColumn() == 6 && to.getRow() == from.getRow() && to.getColumn() == from.getColumn() - 1
-						&& b[to.getColumn()][to.getRow()] == null)
-				|| 
-				// moving forward 1 on first move
-				(from.getColumn() == 6 && to.getRow() == from.getRow() && to.getColumn() == from.getColumn() - 2
-						&& b[to.getColumn()][to.getRow()] == null)
-				||
-				// kill to the right // makes sure you actually want to kill that piece
-				((to.getRow() == from.getRow() + 1 && to.getColumn() == from.getColumn() - 1) && from.getRow() <= 6
-						&& b[to.getColumn()][to.getRow()] != null && b[to.getColumn()][to.getRow()].getPlayer() != getPlayer())
-				||
-				// kill to the left // checking if it is opposing piece
-				((to.getRow() == from.getRow() - 1 && to.getColumn() == from.getColumn() - 1) && from.getRow() >= 1
-						&& b[to.getColumn()][to.getRow()] != null && b[to.getColumn()][to.getRow()].getPlayer() != getPlayer())
-				||
-				// moving forward general
-				(to.getRow() == from.getRow() && to.getColumn() == from.getColumn() - 1
-						&& b[to.getColumn()][to.getRow()] == null)))
-				||
-				//black team
-				(getPlayer() == 2 &&
-				// moving forward general
-				((to.getRow() == from.getRow() && to.getColumn() == from.getColumn() + 1
-						&& b[to.getRow()][to.getColumn()] == null)
-				||
+						&& b[to.getColumn()][to.getRow()] == null) ||
 				// moving forward 2 on first move
-				(from.getColumn() == 1 && to.getRow() == from.getRow() && to.getColumn() == from.getColumn() + 2
-						&& b[to.getColumn()][to.getRow()] == null)
+						(from.getColumn() == 6 && to.getRow() == from.getRow() && to.getColumn() == from.getColumn() - 2
+								&& b[to.getColumn()][to.getRow()] == null)
+						||
+						// kill to the right // makes sure you actually want to kill that piece
+						((to.getRow() == from.getRow() + 1 && to.getColumn() == from.getColumn() - 1)
+								&& from.getRow() <= 6 && b[to.getColumn()][to.getRow()] != null
+								&& b[to.getColumn()][to.getRow()].getPlayer() != getPlayer())
+						||
+						// kill to the left // checking if it is opposing piece
+						((to.getRow() == from.getRow() - 1 && to.getColumn() == from.getColumn() - 1)
+								&& from.getRow() >= 1 && b[to.getColumn()][to.getRow()] != null
+								&& b[to.getColumn()][to.getRow()].getPlayer() != getPlayer())
+						||
+						// moving forward general
+						(to.getRow() == from.getRow() && to.getColumn() == from.getColumn() - 1
+								&& b[to.getColumn()][to.getRow()] == null)))
 				||
-				// "catch and kill" to the right // makes sure you actually want to kill that
-				// piece
-				(from.getRow() <= 6 && b[to.getColumn()][to.getRow()] != null
-						&& b[to.getColumn()][to.getRow()].getPlayer() == 1 && to.getRow() == from.getRow() + 1
-						&& to.getColumn() == from.getColumn() + 1)
-				||
-				// "catch and kill" to the left // makes sure you actually want to kill that
-				// piece
-				(from.getRow() >= 1 && b[from.getColumn() + 1][from.getRow() - 1] != null
-						&& b[from.getColumn() + 1][from.getRow() - 1].getPlayer() == 1
-						&& (to.getRow() == from.getRow() - 1 && to.getColumn() == from.getColumn() + 1))))) 
+				// black team
+				(getPlayer() == 2 &&
+				// moving forward 1 on first move
+						(((from.getColumn() == 1 && to.getRow() == from.getRow()
+								&& to.getColumn() == from.getColumn() + 1 && b[to.getColumn()][to.getRow()] == null) ||
+						// moving forward 2 on first move
+								(from.getColumn() == 1 && to.getRow() == from.getRow()
+										&& to.getColumn() == from.getColumn() + 2
+										&& b[to.getColumn()][to.getRow()] == null)
+								||
+								// "catch and kill" to the right // makes sure you actually want to kill that
+								// piece
+								(from.getRow() <= 6 && b[to.getColumn()][to.getRow()] != null
+										&& b[to.getColumn()][to.getRow()].getPlayer() == 1
+										&& to.getRow() == from.getRow() + 1 && to.getColumn() == from.getColumn() + 1)
+								||
+								// "catch and kill" to the left // makes sure you actually want to kill that
+								// piece
+								(from.getRow() >= 1 && b[from.getColumn() + 1][from.getRow() - 1] != null
+										&& b[from.getColumn() + 1][from.getRow() - 1].getPlayer() == 1
+										&& (to.getRow() == from.getRow() - 1 && to.getColumn() == from.getColumn() + 1)
+										||
+										// moving forward general
+										to.getRow() == from.getRow() && to.getColumn() == from.getColumn() + 1
+												&& b[to.getRow()][to.getColumn()] == null)))))
 			return true;
-		 else 
+		else
 			return false;
-		
+
 	}
 
 }
