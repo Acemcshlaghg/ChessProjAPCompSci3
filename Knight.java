d33763d3777bb8c127f8889fa51a70f4aba0890b
nikolasm2000 <s025888@students.lmsd.org>
2019-03-03 21:31:05 -0500

    Update Knight.java

--- Knight.java
+++ Knight.java
@@ -6,14 +6,14 @@ public class Knight extends Piece{
 	public boolean isValidMove(Location from, Location to, Piece[][] b) {
 		
 		if((b[to.getColumn()][to.getRow()] == null || b[to.getColumn()][to.getRow()].getPlayer() != getPlayer()) && 
-			(to.getColumn()==from.getColumn()+2 && to.getRow()==from.getRow()+1) ||
+			((to.getColumn()==from.getColumn()+2 && to.getRow()==from.getRow()+1) ||
 			(to.getColumn()==from.getColumn()+2 && to.getRow()==from.getRow()-1) ||
 			(to.getColumn()==from.getColumn()-2 && to.getRow()==from.getRow()+1) ||
 			(to.getColumn()==from.getColumn()-2 && to.getRow()==from.getRow()-1) ||
 			(to.getColumn()==from.getColumn()+1 && to.getRow()==from.getRow()+2) ||
 			(to.getColumn()==from.getColumn()+1 && to.getRow()==from.getRow()-2) ||
 			(to.getColumn()==from.getColumn()-1 && to.getRow()==from.getRow()+2) ||
-			(to.getColumn()==from.getColumn()-1 && to.getRow()==from.getRow()-2))
+			(to.getColumn()==from.getColumn()-1 && to.getRow()==from.getRow()-2)))
 			return true;
 		else 
 			return false;
