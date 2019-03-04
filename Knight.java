abe78c457dbe0536b7d66fa45ec011d55fb705de
nikolasm2000 <s025888@students.lmsd.org>
2019-03-03 21:27:35 -0500

    Update Knight.java

--- Knight.java
+++ Knight.java
@@ -4,14 +4,19 @@ public class Knight extends Piece{
 		super(player, "images2/Knight" + player + ".png");
 	}
 	public boolean isValidMove(Location from, Location to, Piece[][] b) {
-		if(b[to.getColumn()][to.getRow()] != null && b[to.getColumn()][to.getRow()].getPlayer() != getPlayer()) {
+		
+		if((b[to.getColumn()][to.getRow()] == null || b[to.getColumn()][to.getRow()].getPlayer() != getPlayer()) && 
+			(to.getColumn()==from.getColumn()+2 && to.getRow()==from.getRow()+1) ||
+			(to.getColumn()==from.getColumn()+2 && to.getRow()==from.getRow()-1) ||
+			(to.getColumn()==from.getColumn()-2 && to.getRow()==from.getRow()+1) ||
+			(to.getColumn()==from.getColumn()-2 && to.getRow()==from.getRow()-1) ||
+			(to.getColumn()==from.getColumn()+1 && to.getRow()==from.getRow()+2) ||
+			(to.getColumn()==from.getColumn()+1 && to.getRow()==from.getRow()-2) ||
+			(to.getColumn()==from.getColumn()-1 && to.getRow()==from.getRow()+2) ||
+			(to.getColumn()==from.getColumn()-1 && to.getRow()==from.getRow()-2))
 			return true;
-		}
-		else if(b[to.getColumn()][to.getRow()] == null) {
-			return true;
-		}
-		else {
+		else 
 			return false;
-		}
+		
 	}
 }
